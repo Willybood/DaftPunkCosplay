05a32b18c5a330ba1c51ac1e21bfc1729cc394dc
Billy Wood <Willybood@yahoo.co.uk>
2023-01-05 21:30:08 +0000

    Debugged installation script

--- Install/install.py
+++ Install/install.py
@@ -7,6 +7,10 @@ import os
 from shutil import copy2
 
 def install_dependencies():
+    print("================== Updating package lists ==================")
+    os.system("sudo apt-get update")
+    print("================== Updated package lists ==================")
+
     print("================== Start Installing PIP ==================")
     os.system("sudo apt install python3-pip -y")
     print("================== Completed Installing PIP ==================")
