4d4cfa773e0e4f8eab51125d52a233f62b65c50d
Billy Wood <Willybood@yahoo.co.uk>
2023-01-14 14:33:17 +0000

    Altered voice changer based on feedback

--- Python/main.py
+++ Python/main.py
@@ -10,7 +10,7 @@ from mouthAnims import runMouthAnims
 from audioMonitor import runAudioMonitor
 
 def runVoiceChanger():
-    os.system("play \"|rec --buffer 2096 -d synth sine fmod 5 band 1.2k 1.5k\"")
+    os.system("play \"|rec --buffer 2096 -d synth sine fmod 5 echos 0.8 0.88 50 0.6\"")
 
 def main():
     voiceChangerThread = threading.Thread(target=runVoiceChanger)
