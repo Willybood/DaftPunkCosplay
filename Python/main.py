2cb14646bd709de07083d01a9e9a9c06a7747371
Billy Wood <Willybood@yahoo.co.uk>
2023-01-17 23:01:07 +0000

    Runs animation on boot

--- Python/main.py
+++ Python/main.py
@@ -8,11 +8,16 @@ import animationPlayer
 import threading
 from mouthAnims import runMouthAnims
 from audioMonitor import runAudioMonitor
+from animationPlayer import playAnimation, clearMatrix
 
 def runVoiceChanger():
     os.system("/usr/bin/play \"|rec --buffer 2096 -d synth sine fmod 5 echos 0.8 0.88 50 0.6\"")
 
 def main():
+    # Play animation on boot
+    playAnimation("/home/pi/DaftPunkCosplay/pixelart animations/Hello.png", 100)
+    clearMatrix()
+
     voiceChangerThread = threading.Thread(target=runVoiceChanger)
     voiceChangerThread.start()
     audioMonitorThread = threading.Thread(target = runAudioMonitor)
