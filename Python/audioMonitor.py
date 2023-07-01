41f7d99e6900250d8e13e90dc6c52f72292bcc55
Billy Wood <Willybood@yahoo.co.uk>
2023-07-01 17:00:36 +0100

    Adapted code for stormtrooper helmet design
    Has LED strips and voice changer but not the LED matrix.

--- Python/audioMonitor.py
+++ Python/audioMonitor.py
@@ -6,6 +6,7 @@
 import time
 from rpi_ws281x import PixelStrip, Color
 import RPi.GPIO as GPIO
+import math
 
 # This function is run once and contains the loop that handles the audiomonitoring
 def runAudioMonitor():
@@ -14,7 +15,7 @@ def runAudioMonitor():
     timeBetweenGpioChecks = 1 # ms
     timeBetweenLedChanges = 100 # ms
     ledCount = 7
-    ledColours = [Color(255, 0, 0), Color(255, 128, 0), Color(255, 255, 0), Color(0, 255, 0), Color(0, 0, 255), Color(76, 0, 153), Color(127, 0, 255)]
+    ledColours = [Color(0, 0, 255), Color(0, 0, 255), Color(0, 0, 255), Color(0, 0, 255), Color(0, 0, 255), Color(0, 0, 255), Color(0, 0, 255)]
     ledFreqHz = 800000   # LED signal frequency in hertz (usually 800khz)
     ledDma = 10          # DMA channel to use for generating signal (try 10)
     ledBrightness = 255  # Set to 0 for darkest and 255 for brightest
@@ -37,17 +38,21 @@ def runAudioMonitor():
             signalDetected = GPIO.input(audioDetectorPin)
         if(secondsSinceLastLedChange > (timeBetweenLedChanges / 1000.0)):
             if(signalDetected):
-                if(currentActiveLed < ledCount):
+                if(currentActiveLed < math.ceil(ledCount / 2)):
                     currentActiveLed = currentActiveLed + 1
             else:
                 if(currentActiveLed > 0):
                     currentActiveLed = currentActiveLed - 1
 
-            for i in range (0, ledCount):
+            for i in range (0, math.ceil(ledCount / 2)):
+                ledToChange1 = math.floor(ledCount / 2) + i
+                ledToChange2 = math.floor(ledCount / 2) - i
                 if(i < currentActiveLed):
-                    strip.setPixelColor(ledCount - (i + 1), ledColours[i])
+                    strip.setPixelColor(ledToChange1, ledColours[ledToChange1])
+                    strip.setPixelColor(ledToChange2, ledColours[ledToChange2])
                 else:
-                    strip.setPixelColor(ledCount - (i + 1), Color(0, 0, 0))
+                    strip.setPixelColor(ledToChange1, Color(0, 0, 0))
+                    strip.setPixelColor(ledToChange2, Color(0, 0, 0))
             strip.show()
             secondsAtLastLedChange = currentSeconds
             signalDetected = False
--- Python/main.py
+++ Python/main.py
@@ -11,18 +11,12 @@ from audioMonitor import runAudioMonitor
 from animationPlayer import playAnimation, clearMatrix
 
 def runVoiceChanger():
-    os.system("/usr/bin/play \"|rec --buffer 2096 -d synth sine fmod 5 echos 0.8 0.88 50 0.6\"")
+    os.system("/usr/bin/play \"|rec --buffer 2096 -d pitch -300 echos 0.8 0.88 100 0.6 150 .5 band 1.2k 1.5k\"")
 
 def main():
-    # Play animation on boot
-    playAnimation("/home/pi/DaftPunkCosplay/pixelart animations/Hello.png", 100)
-    clearMatrix()
-
     voiceChangerThread = threading.Thread(target=runVoiceChanger)
     voiceChangerThread.start()
     audioMonitorThread = threading.Thread(target = runAudioMonitor)
     audioMonitorThread.start()
-    mouthAnimsThread = threading.Thread(target = runMouthAnims)
-    mouthAnimsThread.start()
 
 main()
