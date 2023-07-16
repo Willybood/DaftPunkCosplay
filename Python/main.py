1c2508c19fcf4e5de3759effe692436e7880e5a8
Billy Wood <Willybood@yahoo.co.uk>
2023-07-16 21:44:22 +0100

    Changed audio settings based on testing

--- Python/main.py
+++ Python/main.py
@@ -8,11 +8,11 @@ import threading
 from audioMonitor import runAudioMonitor
 
 def runVoiceChanger():
-    os.system("/usr/bin/play \"|rec --buffer 2096 -d pitch -300 echos 0.8 0.88 100 0.6 150 .5 band 1.2k 1.5k\"")
+    os.system("/usr/bin/play \"|rec --buffer 2096 -d pitch -100 band 1.2k 1.5k\"")
 
 def main():
-    #voiceChangerThread = threading.Thread(target = runVoiceChanger)
-    #voiceChangerThread.start()
+    voiceChangerThread = threading.Thread(target = runVoiceChanger)
+    voiceChangerThread.start()
     audioMonitorThread = threading.Thread(target = runAudioMonitor)
     audioMonitorThread.start()
 
