ccfccfc667f1e7acf480379e289d40fbc908bf37
Billy Wood <Willybood@yahoo.co.uk>
2025-04-12 16:03:56 +0100

    Updated install for newest version of OS

--- Install/install.py
+++ Install/install.py
@@ -16,7 +16,7 @@ def install_dependencies():
     print("================== Completed Installing PIP ==================")
 
     print("================== Start Updating PIP ==================")
-    os.system("sudo pip3 install --upgrade pip")
+    os.system("sudo pip3 install --upgrade pip --break-system-packages")
     print("================== Completed Updating PIP ==================")
 
     print("================== Start Installing Setuptools and Libatlas ==================")
@@ -24,9 +24,9 @@ def install_dependencies():
     print("================== Completed Installing Setuptools and Libatlas ==================")
 
     print("================== Start Installing rpi_ws281x ==================")
-    os.system("sudo pip3 install rpi_ws281x")
+    os.system("sudo pip3 install rpi_ws281x --break-system-packages")
     print("================== Completed Installing rpi_ws281x ==================")
-    
+
     print("================== Start Installing Sox ==================")
     os.system("sudo apt-get install sox -y")
     print("================== Completed Installing Sox ==================")
@@ -36,15 +36,14 @@ def edit_alsa_conf():
     copy2('/usr/share/alsa/alsa.conf', '/usr/share/alsa/alsa.conf.bak')
     print("================== Completed backup of alsa.conf -> alsa.conf.bak ==================")
 
-    print("================== Replacing text in alsa.conf ==================")
-    with open('/usr/share/alsa/alsa.conf', 'r') as file:
-        filedata = file.read()
-        filedata = filedata.replace("defaults.ctl.card 0", "defaults.ctl.card 1")
-        filedata = filedata.replace("defaults.pcm.card 0", "defaults.pcm.card 1")
-    with open('/usr/share/alsa/alsa.conf', 'w') as file:
-        file.write(filedata)
-
-    print("================== Completed replacing text in alsa.conf ==================")
+    #print("================== Replacing text in alsa.conf ==================")
+    #with open('/usr/share/alsa/alsa.conf', 'r') as file:
+        #filedata = file.read()
+        #filedata = filedata.replace("defaults.ctl.card 0", "defaults.ctl.card 1")
+        #filedata = filedata.replace("defaults.pcm.card 0", "defaults.pcm.card 1")
+    #with open('/usr/share/alsa/alsa.conf', 'w') as file:
+        #file.write(filedata)
+    #print("================== Completed replacing text in alsa.conf ==================")
 
 install_dependencies()
 edit_alsa_conf()
